49907cb256b37efa8f01266b94934cc7bb8c3598
thcjatim-bit <EMAIL_GITHUB_ANDA>
2026-08-15 08:21:02 +0700

    fix: use valid PostgreSQL sequence grant syntax

--- database/migrations/2026_08_15_000006_add_material_identity_tables.php
+++ database/migrations/2026_08_15_000006_add_material_identity_tables.php
@@ -62,7 +62,7 @@ return new class extends Migration
         DB::unprepared(<<<'SQL'
             GRANT SELECT ON material_stoks TO pms_app;
             GRANT SELECT, INSERT, UPDATE ON material_sns, drums TO pms_app;
-            GRANT USAGE, SELECT, UPDATE ON SEQUENCES material_sns_id_seq, drums_id_seq TO pms_app;
+            GRANT USAGE, SELECT, UPDATE ON SEQUENCE material_sns_id_seq, drums_id_seq TO pms_app;
 
             CREATE OR REPLACE FUNCTION apply_material_transaction() RETURNS trigger AS $fn$
             DECLARE next_qty numeric(18,3);
