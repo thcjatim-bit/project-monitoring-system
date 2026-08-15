ea5653dd1c7ca90c744d5dd7d99dc52990f7b267
thcjatim-bit <EMAIL_GITHUB_ANDA>
2026-08-15 22:38:21 +0700

    feat: add THC command center request queue (#35)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\CommandCenterController;
 use App\Http\Controllers\MasterDataController;
 use App\Http\Controllers\MaterialInventoryController;
 use App\Http\Controllers\MaterialRequestController;
@@ -17,7 +18,9 @@ Route::middleware('guest')->group(function (): void {
 });
 
 Route::middleware('auth')->group(function (): void {
-    Route::get('/dashboard', fn () => view('dashboard', ['user' => request()->user()]))->middleware('izin:read_dashboard')->name('dashboard');
+    Route::get('/dashboard', [CommandCenterController::class, 'index'])
+        ->middleware(['thc', 'izin:read_dashboard'])
+        ->name('dashboard');
     Route::get('/projects', [ProjectController::class, 'index'])->middleware('izin:read_project')->name('projects.index');
     Route::get('/projects/buat', [ProjectController::class, 'create'])->middleware('izin:create_project')->name('projects.create');
     Route::post('/projects', [ProjectController::class, 'store'])->middleware('izin:create_project')->name('projects.store');
@@ -71,6 +74,7 @@ Route::middleware('auth')->group(function (): void {
     });
     Route::middleware('izin:read_material_request')->group(function (): void {
         Route::get('/material-requests', [MaterialRequestController::class, 'index'])->name('material-requests.index');
+        Route::get('/material-requests/{materialRequest}', [MaterialRequestController::class, 'show'])->name('material-requests.show');
     });
     Route::post('/material-requests', [MaterialRequestController::class, 'store'])
         ->middleware('izin:create_material_request')
